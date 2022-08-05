c22d288954f36ab681ebc4c806fe5f4cb6ea42be
Sayuru Bopitiya <sayururehan77@gmail.com>
2022-08-06 01:02:19 +0530

    ex7 added

---
+++ Practicals/Practical_1/Ex7.java
@@ -0,0 +1,17 @@
+package Practicals.Practical_1;
+
+public class Ex7 {
+    public static void main(String[] args){
+        int a=3, b=3;
+        int sum;
+        double avg;
+
+        sum = a + b;
+        avg = sum/2.0;
+
+        System.out.println("Value of a = " + a);
+        System.out.println("Value of b = " + b);
+        System.out.println("Sum of a and b = " + sum);
+        System.out.println("Average of a and b = " + avg);
+    }
+}
