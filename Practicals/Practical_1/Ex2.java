ff0d3afa2cf6e65d8f1cdcd0c91fc179e09a7a93
Sayuru Bopitiya <sayururehan77@gmail.com>
2022-08-05 23:51:11 +0530

    add practical1

---
+++ Practicals/Practical_1/Ex2.java
@@ -0,0 +1,13 @@
+package Practicals.Practical_1;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("=\tStudent Information\t\t=");
+        System.out.println("==========================================");
+        System.out.println("=Name\t\t:ARDP Ranasinghe\t=");
+        System.out.println("=Reg No\t\t:DIS\\08\\04\\1234\t\t=");
+        System.out.println("=Address\t:Malabe\t\t\t=");
+        System.out.println("==========================================");
+    }   
+}
---
+++ Practicals/Practical_1/Hello.java
@@ -0,0 +1,9 @@
+package Practicals.Practical_1;
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("It's been nice knowing you");
+        System.out.println("Goodbye World!");
+    }
+}
