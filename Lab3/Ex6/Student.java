d30c1b346b7e674bb9c387e4568d8fecd8fc3b67
Sayuru Bopitiya <sayururehan77@gmail.com>
2022-09-08 00:04:00 +0530

    Ex6 lab3

--- Lab3/Ex6/Student.java
+++ Lab3/Ex6/Student.java
@@ -1,3 +1,15 @@
 package Lab3;
 
-public class Student extends 
+public class Student extends Person{
+    String studentId;
+
+    public Student(String newName, String newAddress, String newStudentId){
+        super(newName, newAddress);
+        studentId = newStudentId;
+    }
+
+    void showDetails(){
+        super.showDetails();
+        System.out.println("Student ID: " + studentId);
+    }
+}
--- Lab3/InheritanceDemo.java
+++ Lab3/InheritanceDemo.java
@@ -4,8 +4,11 @@ public class InheritanceDemo {
 
     public static void main(String[] args){
         Person p1 = new Person("John", "123 Main St.");
+        Student s1 = new Student("Mary", "456 Main St.", "IT345");
+
 
         p1.showDetails();        
+        s1.showDetails();
     }
 
 }
